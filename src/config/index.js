46a012fdc7047da6ba586c8c1a7500bb4f3ebebe
Stefan Miodrag <hello@stefanmiodrag.com>
2019-03-28 14:26:18 +0100

    Remove local mongoose database

--- src/config/index.js
+++ src/config/index.js
@@ -2,6 +2,6 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 module.exports = {
-  mongodbUri: "mongodb://localhost:27017/api" || process.env.MONGODB_URI,
+  mongodbUri: process.env.MONGODB_URI,
   port: process.env.PORT || "3000"
 };
